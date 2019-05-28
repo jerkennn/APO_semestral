ae81a0cc8de92296945283d8a29bba165a633cf2
Martin <51007525+martal01@users.noreply.github.com>
2019-05-28 09:46:34 +0200

    Effects

--- led_effects.h
+++ led_effects.h
@@ -12,7 +12,9 @@
 #include <inttypes.h>
 #include <sys/time.h>
 
-void led_animation(int *led, double h_1, double h_2, long int period, long int startTime);
+void led_animation(int *led, double h_1, double h_2, long int period, long int startTime, int on_time, int off_time);
+void led_static(int *led, int red, int green, int blue, int on_time, int off_time);
 uint32_t createRGB(int r, int g, int b);
+
 
 #endif
