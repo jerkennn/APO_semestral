61914a31cdc553e28cde7979f7ee616edb5c41fe
jerkennn <51007343+jerkennn@users.noreply.github.com>
2019-05-29 22:39:24 +0200

    Final version

--- write2lcd_lib.h
+++ write2lcd_lib.h
@@ -1,3 +1,12 @@
+/*******************************************************************
+  KOREK program
+
+  (C) Copyright 2019 by Vojtech Kozel and Martin Rektoris
+      e-mail:   kozelvo1@fel.cvut.cz; rektomar@fel.cvut.cz
+      license:  MIT license
+
+ *******************************************************************/
+
 #ifndef _WRITE2LCD_LIB_H_
 #define _WRITE2LCD_LIB_H_
 #include <stdint.h>
